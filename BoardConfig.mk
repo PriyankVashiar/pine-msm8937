8508c123091db4eff223cf96736c682434903a51
PriyankVashiar <priyankvashiar@gmail.com>
2019-09-10 02:19:38 +0530

    Updated BoardConfig.mk and recovery.fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,3 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2018-2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 DEVICE_PATH := device/xiaomi/pine
 
 # Bootloader
@@ -23,33 +6,34 @@ TARGET_NO_BOOTLOADER := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
 
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_USES_64_BIT_BINDER := false
-
 # Encryption
+TW_INCLUDE_CRYPTO := true
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.console=ttyMSM0 androidboot.usbconfigfs=true BOARD_KERNEL_CMDLINE += androidboot.configfs=true androidboot.selinux=permissive vmalloc=256M firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.console=ttyMSM0 androidboot.usbconfigfs=true BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += buildvariant=user
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_CONFIG := pine_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 TARGET_PREBUILT_KERNEL := device/Xiaomi/pine/kernel
-TARGET_RECOVERY_FSTAB := device/Xiaomi/pine/recovery.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
@@ -58,6 +42,41 @@ BOARD_CACHEIMAGE_PARTITION_SIZE    := 268435456
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 24870924288	# (24870940672−16384 = 24870924288)
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_FSTAB := device/Xiaomi/pine/recovery.fstab
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_USE_TOOLBOX := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_EXTRA_LANGUAGES := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+RECOVERY_SDCARD_ON_DATA := true
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
